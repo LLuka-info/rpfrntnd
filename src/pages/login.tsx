9652afa16a9e82399f3811829c76a017e1ddcf84
LLuka-info <144520883+LLuka-info@users.noreply.github.com>
2025-03-23 20:23:18 +0200

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -66,7 +66,7 @@ export default function LoginPage() {
             className={styles.loginButton}
             
           >
-            Login. Test
+            Login
           </button>
         </form>
       </div>
